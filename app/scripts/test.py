c964079277e3480a3aabb3d8ad76c1787cd9c805
Danila Fedorov <Dan2065@mail.ru>
2022-10-17 15:01:02 +0300

    YOLOX

--- app/scripts/test.py
+++ app/scripts/test.py
@@ -2,6 +2,9 @@ import av
 import os
 import shutil
 import argparse
+import cv2
+import numpy as np
+import glob
 
 
 
@@ -19,10 +22,16 @@ def AddArgParser():
 def ArgParse():
     try:
         args = argParser.parse_args()
-        frames_list = args.frames.split(",")
+        try:
+            
+            frames_list = args.frames.split(",")
+            
+        except:
+            frames_list = "all"
         video = args.video
         return [frames_list, video]
     except:
+        print("Arguments Error")
         raise SystemExit
     
 def ClearDir():
@@ -38,27 +47,64 @@ def SplitVideo(video):
         VideoError()
 
 def SaveFrames(frames_list, splited_video):
-    for frame in frames_list:
-        try:
-            frame = int(frame)
-            splited_video[frame].to_image().save("test/frame_%d.jpg" % splited_video[frame].index)
-        except: 
-            continue
+    if (frames_list != "all"):
+        for frame in frames_list:
+            try:
+                splited_video[frame].to_image().save("test/frame_%d.jpg" % splited_video[frame].index)
+            except: 
+                continue
+    else:
+        for frame in av.open(splited_video).decode():
+            frame.to_image().save("test/frame_%d.jpg" % frame.index)
+
 
 def VideoError():
     print("Video Error")
     raise SystemExit
 
+def SearchObjects():
+    os.system(f"python ../YOLOX/tools/Searching.py \
+    image -f ../YOLOX/exps/default/yolox_s.py -c ../YOLOX/datasets/yolox_s.pth \
+    --path test/ --conf 0.25 --nms 0.45 --tsize 640 \
+    --save_result --device [cpu/gpu]")
+
+def WriteVideo(video_frames):
+    out = cv2.VideoWriter('output_video.avi',cv2.VideoWriter_fourcc(*'DIVX'), 20, (1920, 1080))
+
+    for frame in video_frames:
+        filename = f"processed_frames/frame_{frame}.jpg"
+        print(filename)
+        img = cv2.imread(filename)
+        out.write(img)
+
+    out.release()
+
 def Start():
 
     AddArgParser()
 
+    frame_list = []
+
     params = ArgParse()
+    if (params[0] != "all"):
+        for param in params[0]:
+            param = int(param)
+            frame_list.append(param)
 
     ClearDir()
     
     splited_video = SplitVideo(params[1])
-    SaveFrames(params[0], splited_video)
+    if (params[0] != "all"):
+        SaveFrames(frame_list, splited_video)
+    else: 
+        SaveFrames("all", params[1])
+
+    SearchObjects()
+    if (params[0] != "all"):
+        WriteVideo(frame_list)
+    else: 
+        WriteVideo(splited_video)
+    
 
 Start()
 
